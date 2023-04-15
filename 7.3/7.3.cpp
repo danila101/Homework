c2ded3a892a44b5d2c7e05dd0afdce6da52d2a5b
danila101 <128508048+danila101@users.noreply.github.com>
2023-04-15 09:52:47 +0400

    7.3

---
+++ 7.3/7.3.cpp
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <conio.h>
+
+int calc(int a)
+{
+	for(int i=1;i<a;i++)
+	{
+		if(i*i==a)
+		{
+			return 1;
+		}
+		else if(i*i>a)
+		{
+			return 0;
+		}
+		
+	}
+}
+
+void calc(int *a)
+{
+	for(int i=1;i<(*a);i++)
+	{
+		if(i*i==(*a))
+		{
+			*a=1;
+			break;
+		}
+		else if(i*i>(*a))
+		{
+			*a=0;
+			break;
+		}
+		
+	}
+}
+
+void calc(int &a, int b)
+{
+	for(int i=1;i<a;i++)
+	{
+		if(i*i==a)
+		{
+			a=1;
+			break;
+
+		}
+		else if(i*i>a)
+		{
+			a=0;
+			break;
+
+		}
+		
+	}
+}
+
+main()
+{
+	int a,b,c;
+	
+	printf("Vedite tri chisla:\n");
+	scanf("%i%i%i",&a,&b,&c);
+	
+	a=calc(a);
+	calc(&b);
+	calc(c,a);
+	
+	int s=a+b+c;
+	
+	printf("%i chisla imeyut celye korni\n",s);
+
+	
+
+}
